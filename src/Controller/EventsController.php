ecb96529d31a3f58c69aec6b207ba92e863c73db
macbook <yvesomda93@yahoo.fr>
2021-06-07 20:54:17 +0000

    Just a modification in our eventscontroller

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -1,8 +1,8 @@
 <?php 
 
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
